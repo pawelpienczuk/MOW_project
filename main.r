1de990abf1028975092f6da970cc801b8b393326
Paweł <pawelpienczuk107@wp.pl>
2019-10-21 21:43:42 +0200

    Code and data organization
    Created new branch for developing

--- main.r
+++ main.r
@@ -4,12 +4,16 @@
 rm(list = ls())
 
 training_data <- read.csv("training.csv")
+
+# creating model
 model_appliances <- lm(Appliances~T1+T2+T3+T4+T5+T6+T7+T8+T9+RH_1+RH_2+RH_3+RH_4+RH_5+RH_6+RH_7+RH_8+RH_9+T_out+Press_mm_hg+Windspeed+Visibility+Tdewpoint+rv1+rv2, data = training_data)
 model_lights <- lm(Appliances~T1+T2+T3+T4+T5+T6+T7+T8+T9+RH_1+RH_2+RH_3+RH_4+RH_5+RH_6+RH_7+RH_8+RH_9+T_out+Press_mm_hg+Windspeed+Visibility+Tdewpoint+rv1+rv2, data = training_data)
 
+# printing model's coefs 
 print(model_appliances)
 print(model_lights)
 
+# REDUNDANT - data organization
 complete_data <- read.csv("energydata_complete.csv")
 vars <- data.frame(
   date = substring(complete_data$date,1,10),
