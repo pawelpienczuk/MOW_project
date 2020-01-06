2e2623cebebb9c797f0a1c07b20ca00c3d7008aa
Paweł <pawelpienczuk107@wp.pl>
2020-01-06 17:44:24 +0100

    PLR (piecewise linear regression) added - poor results

--- main.r
+++ main.r
@@ -1,6 +1,7 @@
 # main v1.1
 # linear and reg_trees
-# large RMSE, possible to reduce??
+# large RMSE, possible to reduce??'
+# poor piecewise linear regression
 
 library(rsample)     # data splitting 
 library(dplyr)       # data wrangling
@@ -9,6 +10,7 @@ library(rpart.plot)  # plotting regression trees
 library(ipred)       # bagging
 library(caret)       # bagging
 library(Metrics)     # RMSE
+library(dmr.regtree)  
 
 rm(list = ls())
 
@@ -50,6 +52,7 @@ test_data <- data.frame(
   RH_8 = complete_data$RH_8,
   RH_9 = complete_data$RH_9,
   T_out = complete_data$T_out,
+  RH_out = complete_data$RH_out,
   Press_mm_hg = complete_data$Press_mm_hg,
   Windspeed = complete_data$Windspeed,
   Visibility = complete_data$Visibility,
@@ -92,6 +95,7 @@ vars_train <- data.frame(
   RH_7 = training_data$RH_7,
   RH_8 = training_data$RH_8,
   RH_9 = training_data$RH_9,
+  RH_out = training_data$RH_out,
   T_out = training_data$T_out,
   Press_mm_hg = training_data$Press_mm_hg,
   Windspeed = training_data$Windspeed,
@@ -103,7 +107,7 @@ vars_train <- data.frame(
 
 # creating linear model
 model_linear_app <- lm(
-  formula = Appliances~day_mon+min_hour+T_out+Press_mm_hg+Windspeed+Visibility+Tdewpoint+rv1+rv2,
+  formula = Appliances~day_mon+min_hour+T1+T2+T3+T4+T5+T6+T7+T8+T9+RH_1+RH_2+RH_3+RH_4+RH_5+RH_6+RH_7+RH_8+RH_9+RH_out+T_out+Press_mm_hg+Windspeed+Visibility+Tdewpoint+rv1+rv2,
   data = vars_train
 )
 
@@ -112,7 +116,7 @@ print(model_linear_app)
 
 # regression trees
 m_nobagging <- rpart(
-  formula = Appliances~day_mon+min_hour+T_out+Press_mm_hg+Windspeed+Visibility+Tdewpoint+rv1+rv2,
+  formula = Appliances~day_mon+min_hour+T1+T2+T3+T4+T5+T6+T7+T8+T9+RH_1+RH_2+RH_3+RH_4+RH_5+RH_6+RH_7+RH_8+RH_9+RH_out+T_out+Press_mm_hg+Windspeed+Visibility+Tdewpoint+rv1+rv2,
   data = vars_train,
   method = "anova",
   control = list( xval=5)
@@ -124,21 +128,29 @@ set.seed(123)
 
 # train bagged model
 bagged_m1 <- bagging(
-  formula = Appliances~day_mon+min_hour+T_out+Press_mm_hg+Windspeed+Visibility+Tdewpoint+rv1+rv2,
+  formula = Appliances~day_mon+min_hour+T1+T2+T3+T4+T5+T6+T7+T8+T9+RH_1+RH_2+RH_3+RH_4+RH_5+RH_6+RH_7+RH_8+RH_9+RH_out+T_out+Press_mm_hg+Windspeed+Visibility+Tdewpoint+rv1+rv2,
   data = vars_train,
-  coob = TRUE
+  coob = TRUE,
+  nbagg=20
 )
 
 # BAGGING caret
 
-ctrl <- trainControl(method = "cv", number = 10)
+ctrl <- trainControl(method = "cv", number = 10)      # change from 10
 
 bagged_caret_m1 <- train(
-  Appliances~day_mon+min_hour+T_out+Press_mm_hg+Windspeed+Visibility+Tdewpoint+rv1+rv2,
+  Appliances~day_mon+min_hour+T1+T2+T3+T4+T5+T6+T7+T8+T9+RH_1+RH_2+RH_3+RH_4+RH_5+RH_6+RH_7+RH_8+RH_9+RH_out+T_out+Press_mm_hg+Windspeed+Visibility+Tdewpoint+rv1+rv2,
   data = vars_train,
   method = "treebag",
   trControl = ctrl,
   importance = TRUE
+)
+
+# piecewise linear regression, dmr page 284
+plr_tree <- grow.modtree(
+  formula = Appliances~day_mon+min_hour+T1+T2+T3+T4+T5+T6+T7+T8+T9+RH_1+RH_2+RH_3+RH_4+RH_5+RH_6+RH_7+RH_8+RH_9+RH_out+T_out+Press_mm_hg+Windspeed+Visibility+Tdewpoint+rv1+rv2, 
+  data = vars_train,
+  maxdepth=8
 )
 
 # predicting from test data
@@ -146,9 +158,14 @@ pred_linear <- predict(model_linear_app,test_data)
 pred_nobagging <- predict(m_nobagging,test_data)
 pred_bagged <- predict(bagged_m1,test_data)
 pred_caret <- predict(bagged_caret_m1,test_data)
+pred_plr <- predict(plr_tree,test_data,5)
 
 #evaluating RMSE for each model
 rmse_linear <- rmse(actual=complete_data$Appliances,predicted=pred_linear)
 rmse_nobagging <- rmse(actual=complete_data$Appliances,predicted=pred_nobagging)
 rmse_bagged <- rmse(actual=complete_data$Appliances,predicted=pred_bagged)
 rmse_caret <- rmse(actual=complete_data$Appliances,predicted=pred_caret)
+rmse_plr <- rmse(actual=complete_data$Appliances,predicted=pred_plr)
+
+# plotting most important parameters
+plot(varImp(bagged_caret_m1), 20)
