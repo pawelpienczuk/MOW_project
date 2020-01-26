f8e290ec86c517df9859a41bbe0431dfc2e0fd8a
Paweł <pawelpienczuk107@wp.pl>
2020-01-26 09:05:44 +0100

    plr added

--- R/data_org.R
+++ R/data_org.R
@@ -2,95 +2,113 @@
 # data collecting and organization
 
 # training_data <- read.csv("training.csv")
-complete_data <- read.csv("energydata_complete.csv")
 
-month = as.numeric(substring(complete_data$date,6,7))
-day = as.numeric(substring(complete_data$date,9,10))
-hours = as.numeric(substring(complete_data$date,12,13))
-minutes = as.numeric(substring(complete_data$date,15,16))
-day_mon = day + 30 * month
-min_hour = minutes + 60 * hours
-
-if (target_t == "Appliances"){
-  test_data <- data.frame(
-    Appliances = complete_data$Appliances,
-    month = as.numeric(substring(complete_data$date,6,7)),
-    day = as.numeric(substring(complete_data$date,9,10)),
-    hours = as.numeric(substring(complete_data$date,12,13)),
-    minutes = as.numeric(substring(complete_data$date,15,16)),
-    # day_mon = day_mon,
-    # min_hour = min_hour,
-    T1 = complete_data$T1,
-    T2 = complete_data$T2,
-    T3 = complete_data$T3,
-    T4 = complete_data$T4,
-    T5 = complete_data$T5,
-    T6 = complete_data$T6,
-    T7 = complete_data$T7,
-    T8 = complete_data$T8,
-    T9 = complete_data$T9,
-    RH_1 = complete_data$RH_1,
-    RH_2 = complete_data$RH_2,
-    RH_3 = complete_data$RH_3,
-    RH_4 = complete_data$RH_4,
-    RH_5 = complete_data$RH_5,
-    RH_6 = complete_data$RH_6,
-    RH_7 = complete_data$RH_7,
-    RH_8 = complete_data$RH_8,
-    RH_9 = complete_data$RH_9,
-    T_out = complete_data$T_out,
-    RH_out = complete_data$RH_out,
-    Press_mm_hg = complete_data$Press_mm_hg,
-    Windspeed = complete_data$Windspeed,
-    Visibility = complete_data$Visibility,
-    Tdewpoint = complete_data$Tdewpoint,
-    rv1 = complete_data$rv1,
-    rv2 = complete_data$rv2
-  )  
-} else if(target_t == "lights"){
-  test_data <- data.frame(
-    lights = complete_data$lights,
-    month = as.numeric(substring(complete_data$date,6,7)),
-    day = as.numeric(substring(complete_data$date,9,10)),
-    hours = as.numeric(substring(complete_data$date,12,13)),
-    minutes = as.numeric(substring(complete_data$date,15,16)),
-    day_mon = day_mon,
-    min_hour = min_hour,
-    T1 = complete_data$T1,
-    T2 = complete_data$T2,
-    T3 = complete_data$T3,
-    T4 = complete_data$T4,
-    T5 = complete_data$T5,
-    T6 = complete_data$T6,
-    T7 = complete_data$T7,
-    T8 = complete_data$T8,
-    T9 = complete_data$T9,
-    RH_1 = complete_data$RH_1,
-    RH_2 = complete_data$RH_2,
-    RH_3 = complete_data$RH_3,
-    RH_4 = complete_data$RH_4,
-    RH_5 = complete_data$RH_5,
-    RH_6 = complete_data$RH_6,
-    RH_7 = complete_data$RH_7,
-    RH_8 = complete_data$RH_8,
-    RH_9 = complete_data$RH_9,
-    T_out = complete_data$T_out,
-    RH_out = complete_data$RH_out,
-    Press_mm_hg = complete_data$Press_mm_hg,
-    Windspeed = complete_data$Windspeed,
-    Visibility = complete_data$Visibility,
-    Tdewpoint = complete_data$Tdewpoint,
-    rv1 = complete_data$rv1,
-    rv2 = complete_data$rv2
-  )  
-} else {
-  print("Target can be only lights or Appliances")
-  stop()
+#' Title
+#'
+#' @param data data to be organized
+#' @param target "Appliances" or "lights"
+#' @param testDataLength length of output subset (from 1 do testDataLength)
+#'
+#' @return organised data set (or subset, if testDataLength specified), 
+#'
+dataOrganization <- function(data,target,testDataLength=nrow(data)){
+    
+  #day_mon = day + 30 * month
+  #min_hour = minutes + 60 * hours
+  if (!is.data.frame(data)){
+    message("data is no data.frame type")
+    stop()  
+  }
+  if ((target!="Appliances")&(target!="lights"))
+  {
+    message("Target is not \"Appliances\" neither \"lights\" ")
+    stop()
+  }
+  if (testDataLength<2|testDataLength>nrow(data)){
+    message("testDataLength must be between 2 and length(data)")
+    stop()
+  }
+  
+  
+  if (target == "Appliances"){
+    out_data <- data.frame(
+      Appliances = data$Appliances,
+      month = as.numeric(substring(data$date,6,7)),
+      day = as.numeric(substring(data$date,9,10)),
+      hours = as.numeric(substring(data$date,12,13)),
+      minutes = as.numeric(substring(data$date,15,16)),
+      # day_mon = day_mon,
+      # min_hour = min_hour,
+      T1 = data$T1,
+      T2 = data$T2,
+      T3 = data$T3,
+      T4 = data$T4,
+      T5 = data$T5,
+      T6 = data$T6,
+      T7 = data$T7,
+      T8 = data$T8,
+      T9 = data$T9,
+      RH_1 = data$RH_1,
+      RH_2 = data$RH_2,
+      RH_3 = data$RH_3,
+      RH_4 = data$RH_4,
+      RH_5 = data$RH_5,
+      RH_6 = data$RH_6,
+      RH_7 = data$RH_7,
+      RH_8 = data$RH_8,
+      RH_9 = data$RH_9,
+      T_out = data$T_out,
+      RH_out = data$RH_out,
+      Press_mm_hg = data$Press_mm_hg,
+      Windspeed = data$Windspeed,
+      Visibility = data$Visibility,
+      Tdewpoint = data$Tdewpoint,
+      rv1 = data$rv1,
+      rv2 = data$rv2
+    )  
+  } else if(target == "lights"){
+    out_data <- data.frame(
+      lights = data$lights,
+      month = as.numeric(substring(data$date,6,7)),
+      day = as.numeric(substring(data$date,9,10)),
+      hours = as.numeric(substring(data$date,12,13)),
+      minutes = as.numeric(substring(data$date,15,16)),
+      # day_mon = day_mon,
+      # min_hour = min_hour,
+      T1 = data$T1,
+      T2 = data$T2,
+      T3 = data$T3,
+      T4 = data$T4,
+      T5 = data$T5,
+      T6 = data$T6,
+      T7 = data$T7,
+      T8 = data$T8,
+      T9 = data$T9,
+      RH_1 = data$RH_1,
+      RH_2 = data$RH_2,
+      RH_3 = data$RH_3,
+      RH_4 = data$RH_4,
+      RH_5 = data$RH_5,
+      RH_6 = data$RH_6,
+      RH_7 = data$RH_7,
+      RH_8 = data$RH_8,
+      RH_9 = data$RH_9,
+      T_out = data$T_out,
+      RH_out = data$RH_out,
+      Press_mm_hg = data$Press_mm_hg,
+      Windspeed = data$Windspeed,
+      Visibility = data$Visibility,
+      Tdewpoint = data$Tdewpoint,
+      rv1 = data$rv1,
+      rv2 = data$rv2
+    )  
+  } else {
+    print("Target can be only lights or Appliances")
+    stop()
+  }
+  
+    # checking scirpts on smaller dataset, comment to use full  
+    out_data <- out_data[1:testDataLength,]
+    
+    # unused data to forget
 }
-
-# checking scirpts on smaller dataset, comment to use full  
-test_data <- test_data[1:testDataLength,]
-
-# unused data to forget
-
-rm(complete_data, day_mon, day,min_hour,minutes,month,hours)
--- R/main.r
+++ R/main.r
@@ -16,15 +16,18 @@ target_t <- "lights"
 
 # DATA COLLECTION AND ORGANIZATION GOES HERE
 # 10 to try function, 1000 to test try algorithm, 14803 to full
-testDataLength <- 1000
-
 source('R/data_org.R')
+
+testDataLength <- 1000 
+complete_data <- read.csv("energydata_complete.csv")
+
+test_data <- dataOrganization(complete_data, target_t,testDataLength)
 
 # FEATURE SELECTION GOES HERE
 
 source('R/feature_selection.R')
 
-res <- feature_selection(formula_full = lights~.,
+res <- feature_selection(formula_full = as.formula(paste(target_t, "~.")),
                           target = target_t,
                           test_data = test_data,
                           type = "simple",
@@ -39,9 +42,6 @@ formula <- as.vector(res$attr_part[1])
 
 source("R/model_eval.R")
 
-ctrl <- trainControl(method = "cv", number = 10)
-args_t <- c(method="treebag",trControl=ctrl)
-
 tempdataframe <- model_eval(test_data = test_data,
                             fun = rpart,
                             formula = formula,
@@ -55,11 +55,23 @@ tempdataframe2 <- model_eval(test_data = test_data,
 tempdataframe3 <- model_eval(test_data = test_data,
                              fun = bagging,
                              formula = formula,
-                             crossval_number = 10
+                             crossval_number = 20
 )
-# tempdataframe4 <- model_eval(test_data = test_data,
-#                              fun = train,
-#                              formula = formula,
-#                              crossval_number = 20,
-#                              args=args_t
-# )
+plr_args <- list(minsplit=2, maxdepth=8)
+
+tempdataframe4 <- model_eval(test_data=test_data,
+                             fun = grow.modtree,
+                             formula = formula,
+                             crossval_number = 20,
+                             args = plr_args
+                             )
+
+ctrl <- trainControl(method = "cv", number = 10)
+args_t <- c(method="treebag",trControl=ctrl)
+
+tempdataframe5 <- model_eval(test_data = test_data,
+                             fun = train,
+                             formula = formula,
+                             crossval_number = 20,
+                             args=args_t
+)
